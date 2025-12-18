a1419cec9e36db9dc0012f501bcb249d1ec5ac70
irumvabric <irumvabric#@gmail.com>
2025-12-18 14:49:02 +0200

    Active page

--- resources/views/about/organigramme.blade.php
+++ resources/views/about/organigramme.blade.php
@@ -20,15 +20,15 @@
         <div class="container-fluid">
             <div class="layout-row">
 
-                <!-- SIDEBAR NAV -->
+               <!-- SIDEBAR NAV -->
                 <aside class="sidebar-nav">
                     <h3>Navigation</h3>
                     <nav class="nav flex-column">
-                        <a class="nav-link active" href="{{ route('about.profilabrema') }}">Profil global d'ABREMA</a>
-                        <a class="nav-link" href="{{ route('about.organigramme') }}">Organigramme</a>
-                        <a class="nav-link" href="{{ route('about.equipe') }}">Équipe de Direction</a>
-                        <a class="nav-link" href="{{ route('about.fonction') }}">Fonction Réglementaire</a>
-                        <a class="nav-link" href="{{ route('about.qms') }}">QMS</a>
+                        <a class="nav-link {{ Route::is('about.profilabrema') ? 'active' : '' }}" href="{{ route('about.profilabrema') }}">Profil global d'ABREMA</a>
+                        <a class="nav-link {{ Route::is('about.organigramme') ? 'active' : '' }}" href="{{ route('about.organigramme') }}">Organigramme</a>
+                        <a class="nav-link {{ Route::is('about.equipe') ? 'active' : '' }}" href="{{ route('about.equipe') }}">Équipe de Direction</a>
+                        <a class="nav-link {{ Route::is('about.fonction') ? 'active' : '' }}" href="{{ route('about.fonction') }}">Fonction Réglementaire</a>
+                        <a class="nav-link {{ Route::is('about.qms') ? 'active' : '' }}" href="{{ route('about.qms') }}">QMS</a>
                     </nav>
                 </aside>
 
