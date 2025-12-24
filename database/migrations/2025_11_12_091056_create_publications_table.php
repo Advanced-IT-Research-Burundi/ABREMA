78566668db546e560e255da313f7b6246187f1da
nikezwe <mnikezwe@gmail.com>
2025-12-24 16:03:18 +0200

    fix error publication

--- database/migrations/2025_11_12_091056_create_publications_table.php
+++ database/migrations/2025_11_12_091056_create_publications_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->string('title');
             $table->text('description')->nullable();
             $table->string('image')->nullable();
-            $table->foreignId('user_id')->constrained()->cascadeOnDelete()->nullable();
+            $table->foreignId('user_id')->nullable()->constrained()->cascadeOnDelete();
             $table->timestamps();
             $table->softDeletes();
         });
