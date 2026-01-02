cc7904dfad1377e423e6909824dcb6aee5606990
Jean-Lionel <nijeanlionel@gmailcom>
2026-01-02 12:54:29 +0200

    done

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -104,8 +104,8 @@
                                 <li><a href="{{ route('medicament.produits') }}">Enregistrement</a></li>
                                 <li><a href="{{ route('medicament.notifications') }}">Listes des Notifications</a></li>
                                 <li><a href="{{ route('medicament.textemedicament') }}">Textes Réglementaires</a></li>
-                                <li><a href="{{ route('medicament.listemedicament') }}">Liste Nationale des Médicaments
-                                        Essentiels</a></li>
+                                <!-- <li><a href="{{ route('medicament.listemedicament') }}">Liste Nationale des Médicaments
+                                        Essentiels</a></li> -->
                             </ul>
                         </li>
                         <li class="has-submenu">
